fc3fd8ac40b005a2df8e2210ac3e2f2308525ca4
Michael Rivnak <mrivnak86@gmail.com>
2021-01-14 22:40:53 -0500

    tested clap

--- src/main.rs
+++ src/main.rs
@@ -25,14 +25,6 @@ fn main() {
     const AUTHORS: &'static str = env!("CARGO_PKG_AUTHORS");
     const DESCRIPTION: &'static str = env!("CARGO_PKG_DESCRIPTION");
 
-    // Default settings
-    let settings = Settings {
-        verbose: false,
-        no_confirm: false,
-        runtime: false,
-        show_deps: false
-    };
-
     // Setup file paths
     // TODO: paths
 
@@ -44,19 +36,23 @@ fn main() {
         .arg(Arg::with_name("verbose")
             .short("v")
             .long("verbose")
-            .help("Display verbose output"))
+            .help("Display verbose output")
+            .global(true))
         .arg(Arg::with_name("no-confirm")
             .short("y")
             .long("no-confirm")
-            .help("Accept package updates without prompting the user"))
+            .help("Accept package updates without prompting the user")
+            .global(true))
         .arg(Arg::with_name("runtime")
             .short("r")
             .long("runtime")
-            .help("Display runtime dependencies"))
+            .help("Display runtime dependencies")
+            .global(true))
         .arg(Arg::with_name("show-deps")
             .short("d")
             .long("show-deps")
-            .help("Display package dependencies"))
+            .help("Display package dependencies")
+            .global(true))
         .subcommand(SubCommand::with_name("add")
             .about("Adds package(s) to the system package list")
             .arg(Arg::with_name("PACKAGE")
@@ -78,7 +74,47 @@ fn main() {
         )
         .subcommand(SubCommand::with_name("list")
             .about("Displays the list of installed packages")
+        )
+        .subcommand(SubCommand::with_name("search")
+            .about("Searches for a package")
+            .arg(Arg::with_name("SEARCH")
+                .help("Search term")
+                .required(true)
+            )
+        )
+        .subcommand(SubCommand::with_name("built-with")
+            .about("Checks if one package is build with another")
+            .arg(Arg::with_name("PACKAGE")
+                .help("Package to check")
+                .required(true)
+            )
+            .arg(Arg::with_name("DEPENDENCIES")
+                .help("Dependencies to check against")
+                .required(true)
+                .min_values(1)
+            )
+        )
+        .subcommand(SubCommand::with_name("tail")
+            .about("Displays the last N lines of the log file")
+            .arg(Arg::with_name("LINES")
+                .help("Number of lines to display")
+                .default_value("5")  // Will convert to int later
+                .validator(is_pos_int)
+            )
         );
 
     let matches = app.get_matches();
+
+    let settings = Settings {
+        verbose: matches.is_present("verbose"),
+        no_confirm: matches.is_present("no_confirm"),
+        runtime: matches.is_present("runtime"),
+        show_deps: matches.is_present("show-deps")
+    };
+}
+
+fn is_pos_int(s: String) -> Result<(), String> {
+    let test = s.parse::<u64>().is_ok();
+
+    return if test { return Ok(()); } else { Err(String::from("Value must be a positive integer")) };
 }
